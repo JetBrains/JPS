45690b658a956ac38ad611cbe0c718bf04d12f77
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2017-12-22 22:00:59 +0300

    Migrate to Java: add missing 'public' modifiers

--- plugins/execution-kotlin/src/org/jetbrains/jps/kotlin/KotlinStandaloneScriptLauncher.java
+++ plugins/execution-kotlin/src/org/jetbrains/jps/kotlin/KotlinStandaloneScriptLauncher.java
@@ -25,7 +25,7 @@ import java.util.Map;
  */
 
 public class KotlinStandaloneScriptLauncher extends JavaBasedRunConfigurationLauncher {
-    KotlinStandaloneScriptLauncher() {
+    public KotlinStandaloneScriptLauncher() {
         super("KotlinStandaloneScriptRunConfigurationType");
     }
 /*
