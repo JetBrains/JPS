1125f958221469d227ede87e4c4a115eed23bcde
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2017-12-22 20:53:10 +0300

    Migrate to Java: 'execution-java' module

---
+++ plugins/execution-java/src/org/jetbrains/jps/runConf/java/JavaAppLauncher.java
@@ -0,0 +1,27 @@
+package org.jetbrains.jps.runConf.java;
+
+import org.jetbrains.jps.RunConfiguration;
+
+import java.util.Collections;
+import java.util.List;
+
+class JavaAppLauncher extends JavaBasedRunConfigurationLauncher {
+  public JavaAppLauncher() {
+    super("Application");
+  }
+
+  @Override
+  public String getMainClassName(RunConfiguration runConf) {
+    return runConf.getOption("MAIN_CLASS_NAME");
+  }
+
+  @Override
+  public String getMainClassArguments(RunConfiguration runConf) {
+    return runConf.getOption("PROGRAM_PARAMETERS");
+  }
+
+  @Override
+  public List<String> getMainClassClasspath(RunConfiguration runConf) {
+    return Collections.emptyList();
+  }
+}
